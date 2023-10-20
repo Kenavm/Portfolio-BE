195f5f8dc46a9a6f6721ff381e6a38be0a8896ea
Theresa Grimus <T.Grimus@gmx.net>
2023-10-20 20:26:20 +0200

    added cors

--- src/main/java/com/portfolio/backend/controllers/PortfolioController.java
+++ src/main/java/com/portfolio/backend/controllers/PortfolioController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/portfolio")
+@CrossOrigin(origins = "http://localhost:5173")
 public class PortfolioController {
 
     private final PortfolioService service;
