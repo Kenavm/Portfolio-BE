fb762e7c75be0a64fbf1b113d3e3db87957073a1
Manuel <manuel.vanek@gmail.com>
2023-11-08 17:44:16 +0100

    add column length for aboutDescripition

--- src/main/java/com/portfolio/backend/models/PublicUser.java
+++ src/main/java/com/portfolio/backend/models/PublicUser.java
@@ -1,9 +1,9 @@
 package com.portfolio.backend.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 
 @Entity
 public class PublicUser {
@@ -11,6 +11,7 @@ public class PublicUser {
     @Id
     private long id;
     private String name;
+    @Column(length = 2000)
     private String aboutDescription;
 
 
